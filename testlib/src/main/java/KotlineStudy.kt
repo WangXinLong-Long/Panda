f0b84e607c2abd61010a8a8ca3dd214ea33721ac
wangxinlong <18401564300@126.com>
2017-11-02 17:30:51 +0800

    学习kotlin,菜鸡一个

--- testlib/src/main/java/KotlineStudy.kt
+++ testlib/src/main/java/KotlineStudy.kt
@@ -4,9 +4,12 @@ import jdk.nashorn.internal.objects.Global.println
  * @author wxl
  * @date  on 2017/10/27.
  */
-fun main(args:Array<String>){
-   val a = arrayOf(1,2,3)
-    val b = Array(3,{i->(i*2)})
-    val x:IntArray= intArrayOf(2,3,4)
-    x[0] = x[1]+x[2]
+class Runoob{
+    fun main(args:Array<String>){
+        val a = arrayOf(1,2,3)
+        val b = Array(3,{i->(i*2)})
+        val x:IntArray= intArrayOf(2,3,4)
+        x[0] = x[1]+x[2]
+    }
 }
+
---
+++ testlib/src/main/java/test/Person.kt
@@ -0,0 +1,35 @@
+package test
+
+/**
+ * @author wxl
+ * @date  on 2017/11/2.
+ */
+class Person{
+    var lastName:String="zhang"
+    get() { return field.toUpperCase() }
+    set
+
+    var no:Int = 100
+    get() = field
+    set(value) {
+        if (value <10){
+            field = value
+        }else{
+            field = -1
+        }
+    }
+
+    var height:Float = 145.4f
+    private set
+}
+fun main(agrs:Array<String>){
+    var person:Person = Person()
+    person.lastName = "wang"
+    println("lastName:${person.lastName}")
+
+    person.no = 9
+    println("no:${person.no}")
+
+    person.no=20
+    println("no:${person.no}")
+}
