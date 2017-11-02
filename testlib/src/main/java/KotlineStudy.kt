8832c8893eceb3e8a6b0d3a2626dc374bed59d4b
wangxinlong <18401564300@126.com>
2017-11-02 17:38:11 +0800

    添加备注信息

--- testlib/src/main/java/KotlineStudy.kt
+++ testlib/src/main/java/KotlineStudy.kt
@@ -4,6 +4,7 @@ import jdk.nashorn.internal.objects.Global.println
  * @author wxl
  * @date  on 2017/10/27.
  */
+//TODO:Kotlin的主函数在哪里
 fun main(args:Array<String>){
    val a = arrayOf(1,2,3)
     val b = Array(3,{i->(i*2)})
